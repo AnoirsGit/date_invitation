908a5eca0c03f5f301bda89d24d5751a25a35031
Anoir <anoir@pop-os.localdomain>
2021-09-29 01:49:45 +0600

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main>
+
+	<div class="animation-area">
+		<ul class="box-area">
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+				<li></li>
+		</ul>
+	</div>
+	
+</main>
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const production = !process.env.ROLLUP_WATCH;
+module.exports = {
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  plugins: [
+  ],
+  purge: {
+    content: [
+      "./src/App.svelte",
+    ],
+    enabled: production // disable purge in dev
+  },
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      "black": '#000',
+      "white": '#fff',
+    }
+  }
+};
